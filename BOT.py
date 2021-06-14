669a1e1f8ed0c85a10d4609588e0594a18af4e2c
Matcha769 <85878531+Matcha769@users.noreply.github.com>
2021-06-14 21:55:49 +0800

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,4 +1,9 @@
+import abc
+import contextlib
+from typing import ContextManager
 import discord
+from discord import message
+from discord import channel
 from discord.ext import commands
 from discord.flags import MemberCacheFlags
 
@@ -20,4 +25,10 @@ async def on_member_remove(member):
     channel = bot.get_channel(504579972934139919)
     await channel.send(f'{member} leave!')
 
+@bot.command()
+async def ping(ctx):
+   await ctx.send(f'你的延遲:{round(bot.latency*1000)}(ms)')
+
 bot.run('ODUzOTU4NzM2NTY1NTAxOTcy.YMc9DA.zVu91n7wQY5xZERZU0Fg45MdiF4')
+
+ 
